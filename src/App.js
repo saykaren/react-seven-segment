605d019babf47c28bd069a8170ff0a2570d30ae4
Karen Garcia <karen.garcia@nexscreen.com>
2019-05-11 07:19:04 -0600

    got code to work for input and working moving word

--- src/App.js
+++ src/App.js
@@ -4,48 +4,87 @@ import './App.css';
 import InputToSevenSegments from './Components/InputToSevenSegments';
 import Colors from "./Components/Colors";
 import useInterval from './Helpers/useInterval';
+import SevenSegmentAdapter from './Components/SevenSegmentAdapter';
 
 function App() {
 
   const [counter, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
 
   function incrememt() {
     setCount(counter + 1);
   }
-
   useInterval(incrememt, 1000);
 
-  function wordKaren() {
-    setCount(counter + 10);
+  //trying to make it move 
+  function danceMove() {
+    const result = number+"946";
+    console.log("dance");
+    
   }
 
+  useInterval(danceMove, 5000);
+
+  
+
+  
+
+  function setMyNumber(event) {
+    const karen = event.target.value;
+    console.log("got here");
+    setNumber(karen);
+    if (setNumber.length < 10){
+      let result = " "+karen;
+      setNumber(result);
+    } else {
+      if (setNumber.length > 25) {
+        let resultEnd = karen;
+        setNumber(resultEnd);
+      }
+    }
+
+  }
+
+
   return (
+    // <div className="App">
+    //   <button onClick={incrememt}>Count</button>
+    //   <div className={"container"}>
+    //     <InputToSevenSegments
+    //       number={'#  ' + counter}
+    //       className={'greenglow'}
+    //       colors={Colors.purple}
+    //     />
+    //   </div>
+    // </div>
+    //****** */below does input section 
+      // <div className="App">
+      //   <input type={"text"} value={number} onChange={setMyNumber} />
+      //   <div className={"container"}>
+      //     <InputToSevenSegments
+      //       number={'#  ' + number}
+      //       className={'greenglow'}
+      //       colors={Colors.purple}
+      //     />
+      //   </div>
+      // </div>
     <div className="App">
-      <button onClick={incrememt}>Count</button>
+      <button onClick={danceMove}>Count</button>
       <div className={"container"}>
         <InputToSevenSegments
-          number={'#  ' + counter}
-          className={'greenglow'}
-          colors={Colors.purple}
+        number={"HELLO"}
+        className={"greenglow"}
+        colors={Colors.purple}
         />
       </div>
-    
-      <div className="App"> 
-        <button onClick={wordKaren}>Be Creative</button>
-        <input type="text"></input>
-        <div className={"container"}>
-          <InputToSevenSegments
-            number={"HELLO"}
-          />
-        </div>
-      </div>
     </div>
+    
   );
 
 
 }
 
-const e = React.createElement;
+
 
 
 
